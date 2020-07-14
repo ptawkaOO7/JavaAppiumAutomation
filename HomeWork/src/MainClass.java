6d0166a771263c4221ffc3a88771e372f88b152e
Marina Galkina <ptawka007@Marinas-MacBook-Pro.local>
2020-07-14 22:28:22 +0300

    Initial commit

---
+++ HomeWork/src/MainClass.java
@@ -0,0 +1,21 @@
+public class MainClass
+{
+    public int getLocalNumber()
+    {
+        return 14;
+    }
+
+    private int class_number = 20;
+
+    public int getClassNumber()
+    {
+        return this.class_number;
+    }
+
+    private String class_string = "Hello, world";
+
+    public String getClassString()
+    {
+        return this.class_string;
+    }
+}
---
+++ HomeWork/src/MainClassTest.java
@@ -0,0 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass
+{
+    @Test
+
+    public void testGetLocalNumber()
+    {
+        int a = this.getLocalNumber();
+        Assert.assertTrue("a != 14", a == 14);
+    }
+
+    @Test
+
+    public void testGetClassNumber()
+    {
+        int a = this.getClassNumber();
+        Assert.assertTrue("a <= 45 and equally " + a, a > 45);
+    }
+
+    @Test
+
+    public void testGetClassString()
+    {
+        String a = this.getClassString();
+        Assert.assertTrue("string '" + a + "' doesn't contain 'hello'", a.toLowerCase().contains("hello"));
+    }
+}
