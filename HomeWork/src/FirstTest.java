0c35f47aac1415643f9ec30343a97bd24533a034
ptawka007 <ptawka007@gmail.com>
2020-07-31 15:15:27 +0300

    Ex2

---
+++ HomeWork/src/FirstTest.java
@@ -0,0 +1,85 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+
+public class FirstTest {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName","AndroidTestDevice");
+        capabilities.setCapability("platformVersion","8.0");
+        capabilities.setCapability("automationName","Appium");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("app","/Users/ptawka007/Documents/JavaAppiumAutomation/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+
+    @Test
+    public void testComparePlaceholderText()
+    {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+
+        assertElementHasText(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Search…",
+                "Cannot find placeholder 'Search…' in the search toolbar",
+                5
+        );
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeoutSeconds);
+        element.clear();
+        return element;
+    }
+
+    private WebElement assertElementHasText(By by, String expected_placeholder_text, String error_message, long timeoutSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeoutSeconds);
+        String placeholder_text = element.getAttribute("text");
+        Assert.assertEquals(
+                error_message,
+                expected_placeholder_text,
+                placeholder_text
+         );
+        return element;
+    }
+}
