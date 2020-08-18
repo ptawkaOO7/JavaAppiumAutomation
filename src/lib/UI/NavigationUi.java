0b3f6fe78024472fecf0ab57e2809cf33ff55add
ptawka007 <ptawka007@gmail.com>
2020-08-18 10:12:12 +0300

    workshop

---
+++ src/lib/UI/NavigationUi.java
@@ -0,0 +1,24 @@
+package lib.UI;
+
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+
+public class NavigationUi extends MainPageObject{
+
+    private static final String
+            MY_LISTS_LINK = "//android.widget.FrameLayout[@content-desc='My lists']";
+
+    public NavigationUi(AppiumDriver driver)
+    {
+        super(driver);
+    }
+
+    public void clickMyLists()
+    {
+        this.waitForElementAndClick(
+                By.xpath(MY_LISTS_LINK),
+                "Cannot find navigation button to My list",
+                5
+        );
+    }
+}
---
+++ src/tests/MyListsTests.java
@@ -0,0 +1,35 @@
+package tests;
+
+import lib.CoreTestCase;
+import lib.UI.ArticlePageObject;
+import lib.UI.MyListsPageObject;
+import lib.UI.NavigationUi;
+import lib.UI.SearchPageObject;
+import org.junit.Test;
+
+public class MyListsTests extends CoreTestCase
+{
+    @Test
+    public void testSaveFirstArticleToMyList()
+    {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.clickByArticleWithSubstring("Object-oriented programming language");
+
+        ArticlePageObject ArticlePageObject = new ArticlePageObject(driver);
+        ArticlePageObject.waitForTitleElement();
+        String article_title = ArticlePageObject.getArticleTitle();
+        String name_of_folder = "Learning programming";
+        ArticlePageObject.addArticleToMyList(name_of_folder);
+        ArticlePageObject.closeArticle();
+
+        NavigationUi NavigationUi = new NavigationUi(driver);
+        NavigationUi.clickMyLists();
+
+        MyListsPageObject MyListsPageObject = new MyListsPageObject(driver);
+        MyListsPageObject.openFolderByName(name_of_folder);
+        MyListsPageObject.swipeByArticleToDelete(article_title);
+    }
+}
